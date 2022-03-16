6f6d9f79c676081b7a3fc8f3a22f0631a10c8c33
Gigiphilac <claydiebear@gmail.com>
2022-03-16 16:40:39 +1100

    ideal job

--- common.js
+++ common.js
@@ -1,6 +1,7 @@
 let coll = document.getElementsByClassName("collapsible");
 let cont = document.getElementsByClassName("content");
 
+
 for (let i = 0; i < coll.length; i++) {
     // Execute whenever an accordion is clicked
     coll[i].addEventListener("click", function() {
@@ -14,18 +15,20 @@ for (let i = 0; i < coll.length; i++) {
     });
 }
 
+
 function showPanel(elem) {
     hidePanels();
     elem.classList.add("active");
     elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
-  }
+}
+
 
 function hidePanels() {
     for (let i = 0; i < cont.length; i++) {
         cont[i].style.maxHeight = null;
         coll[i].classList.remove("active");
     }
-  }
+}
 
 
 function changeLogo(){
